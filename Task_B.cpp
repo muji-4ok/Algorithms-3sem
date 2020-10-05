32636c4df7fc098ba56bc23615733ecba6892c43
Egor Kutovoy <egorkincbl@gmail.com>
2020-10-05 18:00:38 +0300

    Deduplicated code

--- Task_B.cpp
+++ Task_B.cpp
@@ -11,63 +11,46 @@ struct SubPalindrome {
   int length;
 };
 
-std::vector<int> CountOddPalindromes(const std::string &s) {
+enum PalindromeType {
+  kOdd,
+  kEven
+};
+
+std::vector<int> CountSubPalindromes(const std::string &s, PalindromeType type) {
   int n = s.size();
   std::vector<int> palidrome_length(n, 0);
-  SubPalindrome right_most_block{0, 0};
+  SubPalindrome right_most_block{-1, 0};
+
+  auto left = [type](int center, int length) -> int {
+    return center - length + (type == kEven);
+  };
+  auto right = [type](int center, int length) -> int {
+    return center + length;
+  };
 
   for (int i = 0; i < n; ++i) {
-    int block_left = right_most_block.center - right_most_block.length;
-    int block_right = right_most_block.center + right_most_block.length;
+    int block_left = left(right_most_block.center, right_most_block.length);
+    int block_right = right(right_most_block.center, right_most_block.length);
     int length = 0;
 
     if (i <= block_right) {
       int mirrored_i = 2 * right_most_block.center - i;
 
-      if (block_left < mirrored_i - palidrome_length[mirrored_i]) {
+      if (block_left < left(mirrored_i, palidrome_length[mirrored_i])) {
         palidrome_length[i] = palidrome_length[mirrored_i];
         continue;
       }
 
-      length = std::min(palidrome_length[mirrored_i], block_right - i);
-    }
-
-    while (0 <= i - length - 1 && i + length + 1 < s.size()
-        && s[i - length - 1] == s[i + length + 1])
-      ++length;
-
-    palidrome_length[i] = length;
-    right_most_block = {i, length};
-  }
-
-  return palidrome_length;
-}
-
-std::vector<int> CountEvenPalindromes(const std::string &s) {
-  int n = s.size();
-  std::vector<int> palidrome_length(n, 0);
-  // Center is lower
-  SubPalindrome right_most_block{-1, 0};
-
-  for (int i = 0; i < n - 1; ++i) {
-    int blockLeft = right_most_block.center - right_most_block.length + 1;
-    int blockRight = right_most_block.center + right_most_block.length;
-    int length = 0;
-
-    if (i <= blockRight) {
-      int mirroredI = 2 * right_most_block.center - i;
-
-      if (blockLeft < mirroredI - palidrome_length[mirroredI] + 1) {
-        palidrome_length[i] = palidrome_length[mirroredI];
-        continue;
+      if (mirrored_i >= 0) {
+        if (type == kOdd)
+          length = std::min(palidrome_length[mirrored_i], block_right - i);
+        else
+          length = std::max(0, std::min(palidrome_length[mirrored_i], block_right - i - 1));
       }
-
-      if (mirroredI >= 0)
-        length = std::max(0, std::min(palidrome_length[mirroredI], blockRight - i - 1));
     }
 
-    while (0 <= i - length && i + length + 1 < s.size()
-        && s[i - length] == s[i + length + 1])
+    while (0 <= left(i, length + 1) && right(i, length + 1) < s.size()
+        && s[left(i, length + 1)] == s[right(i, length + 1)])
       ++length;
 
     palidrome_length[i] = length;
@@ -78,8 +61,8 @@ std::vector<int> CountEvenPalindromes(const std::string &s) {
 }
 
 long long CountSubPalidromes(const std::string &s) {
-  std::vector<int> odd_lengths = CountOddPalindromes(s);
-  std::vector<int> even_lengths = CountEvenPalindromes(s);
+  std::vector<int> odd_lengths = CountSubPalindromes(s, kOdd);
+  std::vector<int> even_lengths = CountSubPalindromes(s, kEven);
   long long result = 0;
 
   for (int l : odd_lengths)
