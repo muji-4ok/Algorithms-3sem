bb62de703907a51bad0d44d9e2fa9ac98e853cd5
Egor Kutovoy <egorkincbl@gmail.com>
2020-12-15 22:21:36 +0300

    Restructured main, created wav file class

--- Task_3_A.cpp
+++ Task_3_A.cpp
@@ -6,7 +6,7 @@
 #include <complex>
 
 // Структура, описывающая заголовок WAV файла.
-struct WAVHEADER {
+struct WavHeader {
   // WAV-формат начинается с RIFF-заголовка:
 
   // Содержит символы "RIFF" в ASCII кодировке
@@ -69,6 +69,50 @@ struct WAVHEADER {
   // Далее следуют непосредственно Wav данные.
 };
 
+// 1 channel, 16 bits per sample
+struct WavFile {
+  explicit WavFile(std::ifstream &in_stream) {
+    in_stream.read(reinterpret_cast<char *>(&header), sizeof(header));
+    data.resize(header.subchunk2_size / sizeof(int16_t));
+    in_stream.read(reinterpret_cast<char *>(data.data()), header.subchunk2_size);
+  }
+
+  void Write(std::ofstream &out_stream) const {
+    out_stream.write(reinterpret_cast<const char *>(&header), sizeof(header));
+    out_stream.write(reinterpret_cast<const char *>(data.data()), data.size() * sizeof(int16_t));
+  }
+
+  void PrintMetadata() const {
+    std::cout << header.chunk_id[0] << header.chunk_id[1] << header.chunk_id[2]
+              << header.chunk_id[3]
+              << '\n';
+    std::cout << "Chunk size: " << header.chunk_size << '\n';
+    std::cout << header.format[0] << header.format[1] << header.format[2] << header.format[3]
+              << '\n';
+    std::cout << header.subchunk1_id[0] << header.subchunk1_id[1] << header.subchunk1_id[2]
+              << header.subchunk1_id[3] << '\n';
+    std::cout << "SubChunkId1: " << header.subchunk1_size << '\n';
+    std::cout << "Audio format: " << header.audio_format << '\n';
+    std::cout << "Channels: " << header.num_channels << '\n';
+    std::cout << "Sample rate: " << header.sample_rate << '\n';
+    std::cout << "Bits per sample: " << header.bits_per_sample << '\n';
+    std::cout << header.subchunk2_id[0] << header.subchunk2_id[1] << header.subchunk2_id[2]
+              << header.subchunk2_id[3] << '\n';
+
+    // Посчитаем длительность воспроизведения в секундах
+    double duration_seconds =
+        1.0 * header.subchunk2_size / (header.bits_per_sample / 8) / header.num_channels
+            / header.sample_rate;
+    int duration_minutes = std::floor(duration_seconds / 60.0);
+    duration_seconds = duration_seconds - (duration_minutes * 60);
+    std::cout << "Duration: " << duration_minutes << ":" << duration_seconds << '\n';
+    std::cout << "Data size: " << header.subchunk2_size << '\n';
+  }
+
+  WavHeader header{};
+  std::vector<int16_t> data;
+};
+
 // rounded up
 size_t UpperPower2(size_t x) {
   size_t ans = 1;
@@ -81,7 +125,7 @@ size_t UpperPower2(size_t x) {
 
 using complex = std::complex<double>;
 
-// s.size() == 2^k
+// a.size() == 2^k
 void FFT(std::vector<complex> &a, bool inverse) {
   if (a.size() == 1)
     return;
@@ -143,46 +187,16 @@ int main() {
     return 1;
   }
 
-  WAVHEADER header;
-  input_file.read(reinterpret_cast<char *>(&header), sizeof(header));
+  WavFile wav(input_file);
 
-  // Выводим полученные данные
-  std::cout << header.chunk_id[0] << header.chunk_id[1] << header.chunk_id[2] << header.chunk_id[3]
-            << '\n';
-  std::cout << "Chunk size: " << header.chunk_size << '\n';
-  std::cout << header.format[0] << header.format[1] << header.format[2] << header.format[3]
-            << '\n';
-  std::cout << header.subchunk1_id[0] << header.subchunk1_id[1] << header.subchunk1_id[2]
-            << header.subchunk1_id[3] << '\n';
-  std::cout << "SubChunkId1: " << header.subchunk1_size << '\n';
-  std::cout << "Audio format: " << header.audio_format << '\n';
-  std::cout << "Channels: " << header.num_channels << '\n';
-  std::cout << "Sample rate: " << header.sample_rate << '\n';
-  std::cout << "Bits per sample: " << header.bits_per_sample << '\n';
-  std::cout << header.subchunk2_id[0] << header.subchunk2_id[1] << header.subchunk2_id[2]
-            << header.subchunk2_id[3] << '\n';
-
-  // Посчитаем длительность воспроизведения в секундах
-  double duration_seconds =
-      1.0 * header.subchunk2_size / (header.bits_per_sample / 8) / header.num_channels
-          / header.sample_rate;
-  int duration_minutes = std::floor(duration_seconds / 60.0);
-  duration_seconds = duration_seconds - (duration_minutes * 60);
-  std::cout << "Duration: " << duration_minutes << ":" << duration_seconds << '\n';
-
-  if (header.bits_per_sample != 16) {
+  if (wav.header.bits_per_sample != 16) {
     std::cout << "Wrong bits per sample\n";
     return 1;
   }
 
-  std::cout << "Data size: " << header.subchunk2_size << '\n';
-
-  std::vector<int16_t> data(header.subchunk2_size / sizeof(int16_t));
-  input_file.read(reinterpret_cast<char *>(data.data()), header.subchunk2_size);
-
   std::cout << "Data is successfully loaded." << '\n';
 
-  std::vector<complex> c_data = ToComplex(data);
+  std::vector<complex> c_data = ToComplex(wav.data);
   FFT(c_data, false);
 
   // Обнуляем 80%
@@ -190,7 +204,7 @@ int main() {
     c_data[i] = 0.0;
 
   FFT(c_data, true);
-  std::vector<int16_t> transformed_data = FromComplex(c_data, data.size());
+  wav.data = FromComplex(c_data, wav.data.size());
 
   const char *output_filename = "speech_result.wav";
   std::ofstream output_file(output_filename);
@@ -200,9 +214,7 @@ int main() {
     return 1;
   }
 
-  output_file.write(reinterpret_cast<char *>(&header), sizeof(header));
-  output_file.write(reinterpret_cast<char *>(transformed_data.data()),
-                    transformed_data.size() * sizeof(int16_t));
+  wav.Write(output_file);
 
   std::cout << "Data transformed successfully\n";
 
